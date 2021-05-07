dbc664672d2fbc7e51b02ed7773cdd9c84180829
Shubham <shubhampharande8@gmail.com>
2021-05-07 21:59:32 +0530

    initial commit

---
+++ js/main.js
@@ -0,0 +1,140 @@
+/*-----------------------------------------------------------------------------------
+/*
+/* Main JS
+/*
+-----------------------------------------------------------------------------------*/  
+
+(function($) {
+
+   /*---------------------------------------------------- */
+	/* Preloader
+	------------------------------------------------------ */ 
+   $(window).load(function() {
+
+      // will first fade out the loading animation 
+    	$("#loader").fadeOut("slow", function(){
+
+        // will fade out the whole DIV that covers the website.
+        $("#preloader").delay(300).fadeOut("slow");
+
+      });     
+
+  	})
+
+   /*----------------------------------------------------*/
+	/*  Placeholder Plugin Settings
+	------------------------------------------------------ */  	 
+	$('input').placeholder() 
+	
+
+   /*----------------------------------------------------- */
+   /* Modals
+   ------------------------------------------------------- */   
+   $('.modal-toggles ul').on('click', 'a', function(e) {
+
+   	var html = $('html'),
+   		 main = $('main, footer'),
+   		 footer = $('footer'),           
+          curMod = $(this).attr('href'),  
+          modal = $(curMod),
+          modClose = modal.find('#modal-close');          
+         
+		main.fadeOut(500, function(){
+			$('html,body').scrollTop(0);
+        	modal.addClass('is-visible');
+      });  
+      
+      e.preventDefault();
+
+      // for old ie
+      if (html.hasClass('oldie')) {
+
+      	$(document).on('click', "#modal-close", function(evt) {
+	      	$('html,body').scrollTop(0); 
+	      	modal.removeClass('is-visible');
+	      	setTimeout(function() {      
+	        		main.fadeIn(500); 
+	        	}, 500);       
+	        	        
+	        	evt.preventDefault();
+      	});
+
+      }
+      // other browsers
+      else {
+
+      	modClose.on('click', function(evt) {
+	      	$('html,body').scrollTop(0); 
+	      	modal.removeClass('is-visible');
+	      	setTimeout(function() {      
+	        		main.fadeIn(500); 
+	        	}, 500);       
+	        	        
+	        	evt.preventDefault();
+	      });
+
+      }     	
+
+   });
+
+   /*---------------------------------------------------- */
+	/* Owl Carousel
+	------------------------------------------------------ */ 
+	$("#owl-slider").owlCarousel({
+        navigation: false,
+        pagination: false,
+        items: 4,
+        navigationText: false,
+		autoPlay:2000,
+		stopOnHover:true,
+		loop: true
+    });
+
+
+   /*----------------------------------------------------*/
+  	/* FitText Settings
+  	------------------------------------------------------ */
+  	setTimeout(function() {
+
+   	  $('main h1, #mod-about h1').fitText(1.1, { minFontSize: '28px', maxFontSize: '38px' });
+
+  	}, 100);
+
+
+   /*---------------------------------------------------- */
+   /* ajaxchimp
+	------------------------------------------------------ */
+
+	// Example MailChimp url: http://xxx.xxx.list-manage.com/subscribe/post?u=xxx&id=xxx
+	var mailChimpURL = 'http://facebook.us8.list-manage.com/subscribe/post?u=cdb7b577e41181934ed6a6a44&amp;id=e65110b38d'
+
+
+	$('#mc-form').ajaxChimp({
+
+		language: 'es',
+	   url: mailChimpURL
+
+	});
+
+	// Mailchimp translation
+	//
+	//  Defaults:
+	//	 'submit': 'Submitting...',
+	//  0: 'We have sent you a confirmation email',
+	//  1: 'Please enter a value',
+	//  2: 'An email address must contain a single @',
+	//  3: 'The domain portion of the email address is invalid (the portion after the @: )',
+	//  4: 'The username portion of the email address is invalid (the portion before the @: )',
+	//  5: 'This email address looks fake or invalid. Please enter a real email address'
+
+	$.ajaxChimp.translations.es = {
+	  'submit': 'Submitting...',
+	  0: '<i class="fa fa-check"></i> We have sent you a confirmation email',
+	  1: '<i class="fa fa-warning"></i> You must enter a valid e-mail address.',
+	  2: '<i class="fa fa-warning"></i> E-mail address is not valid.',
+	  3: '<i class="fa fa-warning"></i> E-mail address is not valid.',
+	  4: '<i class="fa fa-warning"></i> E-mail address is not valid.',
+	  5: '<i class="fa fa-warning"></i> E-mail address is not valid.'
+	} 
+
+})(jQuery);
