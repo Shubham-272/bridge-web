07c76a3ee83cd5cede7ff9efe758eda06d0da7fc
Shubham <shubhampharande8@gmail.com>
2021-05-08 14:17:16 +0530

    Added service call and removed mailchimp

--- js/main.js
+++ js/main.js
@@ -2,139 +2,131 @@
 /*
 /* Main JS
 /*
------------------------------------------------------------------------------------*/  
+-----------------------------------------------------------------------------------*/
 
-(function($) {
+(function ($) {
 
-   /*---------------------------------------------------- */
+	/*---------------------------------------------------- */
 	/* Preloader
-	------------------------------------------------------ */ 
-   $(window).load(function() {
-
-      // will first fade out the loading animation 
-    	$("#loader").fadeOut("slow", function(){
-
-        // will fade out the whole DIV that covers the website.
-        $("#preloader").delay(300).fadeOut("slow");
-
-      });     
-
-  	})
-
-   /*----------------------------------------------------*/
-	/*  Placeholder Plugin Settings
-	------------------------------------------------------ */  	 
-	$('input').placeholder() 
-	
-
-   /*----------------------------------------------------- */
-   /* Modals
-   ------------------------------------------------------- */   
-   $('.modal-toggles ul').on('click', 'a', function(e) {
-
-   	var html = $('html'),
-   		 main = $('main, footer'),
-   		 footer = $('footer'),           
-          curMod = $(this).attr('href'),  
-          modal = $(curMod),
-          modClose = modal.find('#modal-close');          
-         
-		main.fadeOut(500, function(){
-			$('html,body').scrollTop(0);
-        	modal.addClass('is-visible');
-      });  
-      
-      e.preventDefault();
-
-      // for old ie
-      if (html.hasClass('oldie')) {
-
-      	$(document).on('click', "#modal-close", function(evt) {
-	      	$('html,body').scrollTop(0); 
-	      	modal.removeClass('is-visible');
-	      	setTimeout(function() {      
-	        		main.fadeIn(500); 
-	        	}, 500);       
-	        	        
-	        	evt.preventDefault();
-      	});
-
-      }
-      // other browsers
-      else {
-
-      	modClose.on('click', function(evt) {
-	      	$('html,body').scrollTop(0); 
-	      	modal.removeClass('is-visible');
-	      	setTimeout(function() {      
-	        		main.fadeIn(500); 
-	        	}, 500);       
-	        	        
-	        	evt.preventDefault();
-	      });
-
-      }     	
-
-   });
-
-   /*---------------------------------------------------- */
-	/* Owl Carousel
-	------------------------------------------------------ */ 
-	$("#owl-slider").owlCarousel({
-        navigation: false,
-        pagination: false,
-        items: 4,
-        navigationText: false,
-		autoPlay:2000,
-		stopOnHover:true,
-		loop: true
-    });
-
-
-   /*----------------------------------------------------*/
-  	/* FitText Settings
-  	------------------------------------------------------ */
-  	setTimeout(function() {
-
-   	  $('main h1, #mod-about h1').fitText(1.1, { minFontSize: '28px', maxFontSize: '38px' });
-
-  	}, 100);
-
-
-   /*---------------------------------------------------- */
-   /* ajaxchimp
 	------------------------------------------------------ */
+	$(window).load(function () {
 
-	// Example MailChimp url: http://xxx.xxx.list-manage.com/subscribe/post?u=xxx&id=xxx
-	var mailChimpURL = 'http://facebook.us8.list-manage.com/subscribe/post?u=cdb7b577e41181934ed6a6a44&amp;id=e65110b38d'
+		// will first fade out the loading animation 
+		$("#loader").fadeOut("slow", function () {
+
+			// will fade out the whole DIV that covers the website.
+			$("#preloader").delay(300).fadeOut("slow");
+
+		});
+
+	})
+
+	/*----------------------------------------------------*/
+	/*  Placeholder Plugin Settings
+	------------------------------------------------------ */
+	$('input').placeholder()
 
 
-	$('#mc-form').ajaxChimp({
+	/*----------------------------------------------------- */
+	/* Modals
+	------------------------------------------------------- */
+	$('.modal-toggles ul').on('click', 'a', function (e) {
 
-		language: 'es',
-	   url: mailChimpURL
+		var html = $('html'),
+			main = $('main, footer'),
+			footer = $('footer'),
+			curMod = $(this).attr('href'),
+			modal = $(curMod),
+			modClose = modal.find('#modal-close');
+
+		main.fadeOut(500, function () {
+			$('html,body').scrollTop(0);
+			modal.addClass('is-visible');
+		});
+
+		e.preventDefault();
+
+		// for old ie
+		if (html.hasClass('oldie')) {
+
+			$(document).on('click', "#modal-close", function (evt) {
+				$('html,body').scrollTop(0);
+				modal.removeClass('is-visible');
+				setTimeout(function () {
+					main.fadeIn(500);
+				}, 500);
+
+				evt.preventDefault();
+			});
+
+		}
+		// other browsers
+		else {
+
+			modClose.on('click', function (evt) {
+				$('html,body').scrollTop(0);
+				modal.removeClass('is-visible');
+				setTimeout(function () {
+					main.fadeIn(500);
+				}, 500);
+
+				evt.preventDefault();
+			});
+
+		}
 
 	});
 
-	// Mailchimp translation
-	//
-	//  Defaults:
-	//	 'submit': 'Submitting...',
-	//  0: 'We have sent you a confirmation email',
-	//  1: 'Please enter a value',
-	//  2: 'An email address must contain a single @',
-	//  3: 'The domain portion of the email address is invalid (the portion after the @: )',
-	//  4: 'The username portion of the email address is invalid (the portion before the @: )',
-	//  5: 'This email address looks fake or invalid. Please enter a real email address'
+	/*---------------------------------------------------- */
+	/* Owl Carousel
+	------------------------------------------------------ */
+	$("#owl-slider").owlCarousel({
+		navigation: false,
+		pagination: false,
+		items: 4,
+		navigationText: false,
+		autoPlay: 2000,
+		stopOnHover: true,
+		loop: true
+	});
 
-	$.ajaxChimp.translations.es = {
-	  'submit': 'Submitting...',
-	  0: '<i class="fa fa-check"></i> We have sent you a confirmation email',
-	  1: '<i class="fa fa-warning"></i> You must enter a valid e-mail address.',
-	  2: '<i class="fa fa-warning"></i> E-mail address is not valid.',
-	  3: '<i class="fa fa-warning"></i> E-mail address is not valid.',
-	  4: '<i class="fa fa-warning"></i> E-mail address is not valid.',
-	  5: '<i class="fa fa-warning"></i> E-mail address is not valid.'
-	} 
+
+	/*----------------------------------------------------*/
+	/* FitText Settings
+	------------------------------------------------------ */
+	setTimeout(function () {
+
+		$('main h1, #mod-about h1').fitText(1.1, { minFontSize: '28px', maxFontSize: '38px' });
+
+	}, 100);
+
+
+	/*---------------------------------------------------- */
+	/* form post request
+	 ------------------------------------------------------ */
+	$("#form").submit(function (e) {
+
+		e.preventDefault();
+
+		var actionUrl = "https://bridge-mail-list-server.herokuapp.com/addEmail";
+
+		$.ajax({
+			url: actionUrl,
+			type: 'post',
+			contentType: 'application/x-www-form-urlencoded',
+			data: $(this).serialize(),
+			success: function (data, textStatus, jQxhr) {
+				$('#server-response').html('<i class="fa fa-check"></i>' + data.message);
+				$('#server-response').css('display', 'block');
+				$('#signup').css('margin-bottom', '19px');
+			},
+			error: function (jqXhr, textStatus, errorThrown) {
+				$('#server-response').html('<i class="fa fa-warning"></i>' + errorThrown.message);
+				$('#server-response').css('display', 'block');
+				$('#signup').css('margin-bottom', '19px');
+				console.log(errorThrown.message);
+			}
+		})
+	})
 
 })(jQuery);
